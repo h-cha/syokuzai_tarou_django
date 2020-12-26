3d6b47fb30eecc24592f7306a527c30b96ef16ae
h-cha <cocoa0331oreo@gmail.com>
2020-12-27 03:46:54 +0900

    heroku_1227

--- syokuzai_tarou/settings.py
+++ syokuzai_tarou/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS =(
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #追加
 
