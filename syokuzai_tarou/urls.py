ff0576e220d3e0ce5fa89b0d91252a8820bc8d1f
h-cha <cocoa0331oreo@gmail.com>
2020-12-27 13:12:24 +0900

    heroku_1227

--- syokuzai_tarou/urls.py
+++ syokuzai_tarou/urls.py
@@ -28,6 +28,5 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('recipe/',include('recipe.urls')),
-    path('staticfiles/refrigerator/css/table.css'),
 ]
 
