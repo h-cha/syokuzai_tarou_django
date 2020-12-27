3b9eb1badbae6bf040bbc4b91e74f59d33d5e77b
h-cha <cocoa0331oreo@gmail.com>
2020-12-27 12:19:26 +0900

    heroku_1227

--- syokuzai_tarou/settings.py
+++ syokuzai_tarou/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS =(
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #追加
 
